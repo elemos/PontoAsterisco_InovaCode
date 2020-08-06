d80eb1c6f3a5aaadc778d9c526f163d6862f6f3f
elemos <moonjdemon_007@yahoo.com.br>
2020-08-05 23:42:40 -0300

    Commit do Etherpad

---
+++ nb-plugin/build/classes-generated/org/pa/Bundle.java
@@ -0,0 +1,12 @@
+package org.pa;
+/** Localizable strings for {@link org.pa}. */
+class Bundle {
+    /**
+     * @return <i>Me Ajuda</i>
+     * @see VerOnTheLine
+     */
+    static String CTL_VerOnTheLine() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_VerOnTheLine");
+    }
+    private Bundle() {}
+}
